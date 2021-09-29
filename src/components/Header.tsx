3ed89a42be4ac4a60b18a5569133504c974b0b16
cwiemeyer <chrisx862@gmail.com>
2021-09-29 19:36:40 +0200

    Initial commit of github browse and search tool based on Github V4 GraphQL API

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <h1>GitHub React Issue Search Helper</h1>
+                <h2>
+                    Search GitHub React issues and get the search results easily displayed in a list within this page.
+                </h2>
+            </header>
+        );
+    }
+}
+
+export default Header;
